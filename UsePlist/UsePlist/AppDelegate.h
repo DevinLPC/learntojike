c24a7c3c67b3201ce06110fc8ccfb8d80859a316
mayuanyi853 <mayuanyi853@163.com>
2016-06-30 16:05:58 +0800

    Property List

---
+++ UsePlist/UsePlist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UsePlist
+//
+//  Created by mayee on 16/6/30.
+//  Copyright © 2016年 jikexueyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
