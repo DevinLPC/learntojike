705182ef59c8a26c7c57cfb462904ee819e82bba
mayuanyi853 <mayuanyi853@163.com>
2016-07-05 09:47:52 +0800

    aotulayout 自动布局

---
+++ MyAotuLayout/MyAotuLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAotuLayout
+//
+//  Created by mayee on 16/7/4.
+//  Copyright © 2016年 jikexueyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
